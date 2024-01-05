c5cd31c82d10921a7cc9616aa9522c123d4795e2
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-01-05 09:29:43 +0530

    hero page created

---
+++ components/CustomButton.tsx
@@ -0,0 +1,18 @@
+import { CustomButtonProps } from "@/types"
+
+const CustomButton = ({ title, containerStyle, handleClick}: CustomButtonProps ) => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyle}`}
+      onClick={handleClick}
+    >
+      <span className="flex-1">
+        {title}
+      </span>
+    </button>
+  )
+}
+
+export default CustomButton
