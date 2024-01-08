d034d03b97fb2ba465e6891c052f0de8c6b0ce95
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-01-08 20:09:19 +0530

    combox box complete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import Image from "next/image"
-import { CustomButton } from "."
+import { CustomButton } from "./"
 
 const Navbar = () => {
   return (
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export interface CustomButtonProps {
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
 }
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacture: string) => void;
+}
